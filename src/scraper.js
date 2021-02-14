ff1000784c3e144110f0152c6f4c164d463981ed
Alberto Zanotti <albizan@hotmail.it>
2021-02-14 17:22:08 +0100

    Limit asin

--- src/scraper.js
+++ src/scraper.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const baseQuery = "https://www.amazon.it/s?k=";
 
-async function scrapeAsins(keywords) {
+async function scrapeAsins(keywords, limit = 5) {
   try {
     const asins = new Set();
     const browser = await puppeteer.launch({
@@ -9,20 +9,25 @@ async function scrapeAsins(keywords) {
     });
     const page = await browser.newPage();
     for (const keyword of keywords) {
-      console.info(`Loading page for keyword: ${keyword}`);
+      let asinAmount = 0;
       await page.goto(baseQuery + keyword.replace(" ", "+"));
       await page.waitForSelector(".a-link-normal.a-text-normal", { timeout: 5000 });
       const scrapedAsins = await page.$$eval("[data-asin]", (elements) => elements.map((div) => div.getAttribute("data-asin")));
-
+      if (scrapedAsins.length > limit) {
+        scrapedAsins.length = limit;
+      }
       // Add scraped asins to asin set
       scrapedAsins.forEach((scrapedAsin) => {
-        if (scrapedAsin) {
+        if (scrapedAsin.length === 10 && scrapedAsin.charAt(0) === "B") {
           asins.add(scrapedAsin);
+          asinAmount++;
         }
       });
+
+      console.info(`Scraped page for keyword: ${keyword}. Found ${asinAmount} asins`);
     }
 
-    console.info(`Asins found: ${asins.size}`);
+    console.info(`Distinct asins found: ${asins.size}`);
     browser.close();
     return asins;
   } catch (err) {
